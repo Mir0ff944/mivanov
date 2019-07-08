e2dce075791466dbf133c86657b7cc1f91622757
Miroslav Ivanov <mivanovuk5@gmail.com>
2019-07-08 09:25:44 +0100

    - init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          {/* <li>
+            <Link to="/work">About</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">Topics</Link>
+          </li> */}
+        </ul>
+
+        <hr />
+
+        {/* <Route exact path="/" component={Home} />
+        <Route path="/work" component={myWork} />
+        <Route path="/portfolio" component={Portfolio} /> */}
+      </div>
+    </Router>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
