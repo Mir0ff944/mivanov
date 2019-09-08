0cbc9c05550f3d2d65e9b56d0993564f204139a2
Miroslav Ivanov <mivanovuk5@gmail.com>
2019-09-08 22:52:22 +0100

    - animated sidebar and children, some basic styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,43 @@
 import React, { Suspense, lazy} from 'react';
 import { Router, Link } from "@reach/router";
 import Social from './sidenav/Social';
+import posed from 'react-pose';
 
 // Component imports
 const Home = lazy(() => import('./Home'));
 const Resume = lazy(() => import('./Resume'));
 const NotFound = () => <p>Sorry, nothing here</p>
 
+// Pose sidebar items
+const Sidebar = posed.ul({
+  open: { x: '0%', delayDchildren: 200, staggerChildren: 100 },
+  closed: { x: '-110%', delay: 300 }
+})
+
+const Children = posed.li({
+  open: { y: 0, opacity: 1 },
+  closed: { y: 20, opacity: 1 }
+})
+
 export default class Naigation extends React.Component {
+  state = { isOpen: false}
+
+  toggleNav = () => this.setState({isOpen: !this.state.isOpen});
+
+  componentDidMount() {
+    setTimeout(this.toggleNav, 1000)
+  }
 
   render() {
+    const { isOpen } = this.state;
+
     return(
       <div>
         <div className="navbar__wrapper">
-          <ul className="navbar__list">
-            <li className="navbar__list-item"><Link to="/">Home</Link></li>
-            <li className="navbar__list-item"><Link to="/resume">Resume</Link></li>
-          </ul>
+          <Sidebar className="navbar__list" pose={isOpen ? 'open' : 'closed' }>
+            <Children className="navbar__list-item"><Link to="/">Home</Link></Children>
+            <Children className="navbar__list-item"><Link to="/resume">Resume</Link></Children>
+          </Sidebar>
           <Social />
         </div>
         <div className="main-content container">
